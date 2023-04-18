51005d30dbcd9ef8157a64b1226435d322f930f5
Prashant Goel <56596612+Prashant1806@users.noreply.github.com>
2023-04-19 01:33:28 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+# Constants
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+SCREENSHOT_DIR = os.path.join(BASE_DIR, 'screenshots')
+REPORT_DIR = os.path.join(BASE_DIR, 'reports')
+
+# Instagram API configuration
+INSTAGRAM_API_BASE_URL = 'https://api.instagram.com/v1/'
+INSTAGRAM_ACCESS_TOKEN = 'your-access-token-here'
+
+# Bot configuration
+BOT_USERNAME = 'your-bot-username-here'
+BOT_PASSWORD = 'your-bot-password-here'
